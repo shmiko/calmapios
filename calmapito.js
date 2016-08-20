a1b8e838063f66f61836705c23efdfaba5d3bb6f
shmiko <shmiko@gmail.com>
2016-08-20 15:56:29 +1000

    collapse sidebar

--- calmapito.js
+++ calmapito.js
@@ -24,7 +24,40 @@
 			,{"stylers":[{"saturation":-17},{"gamma":0.36}]}
 			,{"featureType":"transit.line","elementType":"geometry","stylers":[{"color":"#3f518c"}]}
 			];	
-	
+
+
+	     function CenterControl(controlDiv, map) {
+
+        // Set CSS for the control border.
+        var controlUI = document.createElement('div');
+        controlUI.style.backgroundColor = '#fff';
+        controlUI.style.border = '2px solid #fff';
+        controlUI.style.borderRadius = '3px';
+        controlUI.style.boxShadow = '0 2px 6px rgba(0,0,0,.3)';
+        controlUI.style.cursor = 'pointer';
+        controlUI.style.marginBottom = '22px';
+        controlUI.style.textAlign = 'center';
+        controlUI.title = 'Click to toggle sidebar';
+        controlDiv.appendChild(controlUI);
+
+        // Set CSS for the control interior.
+        var controlText = document.createElement('div');
+        controlText.style.color = 'rgb(25,25,25)';
+        controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+        controlText.style.fontSize = '16px';
+        controlText.style.lineHeight = '38px';
+        controlText.style.paddingLeft = '5px';
+        controlText.style.paddingRight = '5px';
+        controlText.innerHTML = 'Center Map';
+        controlUI.appendChild(controlText);
+
+        // Setup the click event listeners: simply set the map to Chicago.
+        controlUI.addEventListener('click', function() {
+          //map.setCenter(chicago);
+          document.getElementsByClassName('options-box')[0].classList.toggle('collapsed')
+        });
+
+      }
   function initMap() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
@@ -33,6 +66,14 @@
       styles: featureOpts,
       mapTypeControl: false
     });
+
+    // Create the DIV to hold the control and call the CenterControl()
+    // constructor passing in this DIV.
+    var centerControlDiv = document.createElement('div');
+    var centerControl = new CenterControl(centerControlDiv, map);
+
+    centerControlDiv.index = 1;
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(centerControlDiv);
     // var opera_house = {lat: -33.856159, lng: 151.215256};
     // var oldMarker = new google.maps.Marker({
     //   position: opera_house,
